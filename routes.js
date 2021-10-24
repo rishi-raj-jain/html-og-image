4fd786342e70ea582dc38740dfdb9aea6ba8efca
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2021-10-24 23:56:58 +0530

    Cache API Routes on the edge forever

--- routes.js
+++ routes.js
@@ -5,4 +5,15 @@ module.exports = new Router()
   .match('/service-worker.js', ({ serviceWorker }) => {
     return serviceWorker('.next/static/service-worker.js')
   })
+  .match('/api', ({ cache }) => {
+    cache({
+      browser: {
+        maxAgeSeconds: 31536000,
+        serviceWorkerSeconds: 31536000,
+      },
+      edge: {
+        maxAgeSeconds: 31536000,
+      },
+    })
+  })
   .use(nextRoutes)
