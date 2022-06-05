e4977591e012f918103c50a3a48b0d91408e633a
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-06-05 18:59:39 +0000

    GET -> POST

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,28 +2,30 @@ import core from 'puppeteer-core'
 import chromium from 'chrome-aws-lambda'
 
 export default async function handler(req, res) {
-  let { title, mode, image, width = 1400, height = 720 } = req.query
-  mode= decodeURIComponent(mode)
-  title= decodeURIComponent(title)
-  image= decodeURIComponent(image)
-  const browser = await core.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  })
-  const page = await browser.newPage()
-  await page.setViewport({ width: parseInt(width), height: parseInt(height) })
-  await page.waitForTimeout(2000)
-  await page.goto(`https://rishi-raj-jain-html-og-image-default.layer0-limelight.link/blogs?title=${title}&image=${image}&mode=${mode}`)
-  await page.waitForTimeout(6000)
-  const content = await page.$('body')
-  const imageBuffer = await content.screenshot({ omitBackground: true })
-  await page.close()
-  await browser.close()
-  res.setHeader('Cache-Control', 'public, immutable, no-transform, s-maxage=31536000, max-age=31536000')
-  res.setHeader('Content-Type', 'image/png')
-  res.send(imageBuffer)
-  res.status(200)
+  // Only allow POST to the given route
+  if (req.method === 'POST') {
+    const { title, mode, image, width = 1400, height = 720 } = req.body
+    const browser = await core.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    })
+    const page = await browser.newPage()
+    await page.setViewport({ width: parseInt(width), height: parseInt(height) })
+    await page.goto(`https://rishi-raj-jain-html-og-image-default.layer0-limelight.link/blogs?title=${title}&image=${image}&mode=${mode}`)
+    await page.waitForTimeout(5000)
+    const content = await page.$('body')
+    const imageBuffer = await content.screenshot({ omitBackground: true })
+    await page.close()
+    await browser.close()
+    res.setHeader('Cache-Control', 'public, immutable, no-transform, s-maxage=31536000, max-age=31536000')
+    res.setHeader('Content-Type', 'image/png')
+    res.send(imageBuffer)
+    res.status(200)
+    return
+  }
+  res.status(400).json({ message: 'Invalid method.' })
+  return
 }
--- routes.js
+++ routes.js
@@ -1,23 +1,11 @@
-const { Router } = require('@layer0/core/router')
 const { nextRoutes } = require('@layer0/next')
+const { Router } = require('@layer0/core/router')
 
 module.exports = new Router()
   .match('/service-worker.js', ({ serviceWorker }) => {
     return serviceWorker('.next/static/service-worker.js')
   })
   .match('/blogs', ({ cache }) => {
-    cache({
-      browser: {
-        maxAgeSeconds: 0,
-        serviceWorkerSeconds: 31536000,
-      },
-      edge: {
-        maxAgeSeconds: 31536000,
-        forcePrivateCaching: true,
-      },
-    })
-  })
-  .match('/api', ({ cache }) => {
     cache({
       browser: {
         maxAgeSeconds: 0,
