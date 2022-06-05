b7c52614c75225142dce408b1dff1c839f1dda58
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-06-06 01:00:10 +0530

    Update blogs.js

--- pages/blogs.js
+++ pages/blogs.js
@@ -27,9 +27,6 @@ export async function getServerSideProps({ query }) {
     image = 'https://images.unsplash.com/photo-1579123521334-44e68095cd7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
     mode = 'false',
   } = query
-  title= decodeURIComponent(title)
-  image= decodeURIComponent(image)
-  mode= decodeURIComponent(mode)
   return {
     props: {
       title,
